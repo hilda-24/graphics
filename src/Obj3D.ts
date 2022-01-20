7508b9cee358db77232dca7c296bd76fd804e4db
hilda-24 <hildacozo24@gmail.com>
2022-01-20 12:05:48 -0600

    Update Obj3D.ts

--- src/Obj3D.ts
+++ src/Obj3D.ts
@@ -1,65 +1,227 @@
-importar  {  Point3D  }  desde  './Point3D.js' ;
+// Obj3D.java: A 3D object and its 2D representation.
+// Uses: Point2D (Section 1.5), Point3D (Section 3.9),
+//       Polygon3D, Input (Section 5.5).
+import { Point2D } from './Point2D.js';
+import { Point3D } from './Point3D.js';
+import { Input } from './Input.js';
+import { Polygon3D } from './Polygon3D.js';
+import { Dimension } from './Dimension.js';
+import { CvHLines } from './CvHLines';
 
-exportar  clase  Rota3D {
-  estático  r11 : número ;  estático  r12 : número ;  estático  r13 : número ;
-  estático  r21 : número ;  r22 estático  : número ; estático r23 : número ;  
-  estático  r31 : número ;  estático  r32 : número ;  estático  r33 : número ;
-  estático  r41 : número ;  estático  r42 : número ;  estático  r43 : número ;
+export class Obj3D{
+   rho: number; d: number; theta: number = 0.30; phi: number = 1.3; rhoMin: number; rhoMax: number;
+   xMin: number; xMax: number; yMin: number; yMax: number; zMin: number; zMax: number;
+   v11: number; v12: number; v13: number; v21: number; v22: number; v23: number; v32: number;
+   v33: number; v43: number; xe: number; ye: number; ze: number; objSize: number;
+   private imgCenter: Point2D;
+   private sunZ: number = 1 / Math.sqrt(3); sunY: number = this.sunZ; sunX: number = -this.sunZ;
+   inprodMin: number = 1e30; inprodMax: number = -1e30; inprodRange: number;
+   public w:any[] = new Array();         // World coordinates
+   private e:Array<Point3D>;                     // Eye coordinates
+   private vScr: Array<Point2D>;//Point2D[];                  // Screen coordinates
+   private polyList:any[] = new Array();  // Polygon3D objects 
+   private file: string = " ";
+   public indices:Array<number> = [];
+	public tind:number=0;           // File name
 
-/* El método initRotate calcula la matriz de rotación general
-            | r11 r12 r13 0 |
-        R = | r21 r22 r23 0 |
-            | r31 r32 r33 0 |
-            | r41 r42 r43 1 |
-   para ser usado como [x1 y1 z1 1] = [xyz 1] R
-   por el método 'rotar'.
-   El punto (x1, y1, z1) es la imagen de (x, y, z).
-   La rotación tiene lugar alrededor del eje dirigido.
-   AB y por el ángulo alfa.
-*/
-   initRotate estático ( a : Point3D ,  b : Point3D ,  alpha : number ) : void {
-    Sea  v1  =  b . x  -  a . x ,
-        v2  =  segundo . y  -  a . y ,
-        v3  =  segundo . z  -  a . z ,
-        theta  =  Matemáticas . atan2 ( v2 ,  v1 ) ,
-        phi  =  Matemáticas . atan2 ( Math . sqrt ( v1  *  v1  +  v2  *  v2 ) ,  v3 ) ;
-        esto _ initRotate2 ( a ,  theta ,  phi ,  alfa ) ;
-  }
+   read(file:any ): boolean {
+      let inp:Input  = new Input(file);
+      if (inp.fails())
+         return this.failing();
+      this.file = file;
+      this.xMin = this.yMin = this.zMin = +1e30;
+      this.xMax = this.yMax = this.zMax = -1e30;
+      return this.readObject(inp); // Read from inp into obj
+   }
 
-   initRotate2 estático ( a : Point3D ,  theta : número ,  phi : número ,  alfa : número ) : void {
-    sea  cosAlpha : número ,  sinAlpha : número ,  cosPhi : número ,  sinPhi : número ,
-      cosTheta : número ,  sinTheta : número ,  cosPhi2 : número ,  sinPhi2 : número ,
-      cosTheta2 : número ,  sinTheta2 : número ,  pi ,  c : número ,
-      a1  =  un . x ,  a2  =  un . y ,  a3  =  un . z ;
-      cosPhi  =  Matemáticas . cos ( fi ) ;  senPhi  =  Matemáticas . pecado ( fi ) ;
-      cosPhi2  =  cosPhi  *  cosPhi ;  sinPhi2  =  sinPhi  *  sinPhi ;
-      cosTheta  =  Matemáticas . cos ( theta ) ;
-      sinTheta  =  Matemáticas . pecado ( theta ) ;
-      cosTheta2  =  cosTheta  *  cosTheta ;
-      sinTheta2  =  sinTheta  *  sinTheta ;
-      cosAlfa  =  Matemáticas . cos ( alfa ) ;
-      sinAlfa  =  Matemáticas . pecado ( alfa ) ;
-      c  =  1.0  -  cosAlfa ;
-      esto _ r11  =  cosTheta2  *  ( cosAlpha  *  cosPhi2  +  sinPhi2 )
-            +  cosAlfa  *  sinTheta2 ;
-      esto _ r12  =  sinAlpha  *  cosPhi + c *  sinPhi2  *  cosTheta  *  sinTheta ;
-      esto _ r13  =  sinPhi  *  ( cosPhi  *  cosTheta * c -  sinAlpha  *  sinTheta ) ;
-      esto _ r21  =  sinPhi2  *  cosTheta  *  sinTheta * c - sinAlpha  *  cosPhi ;
-      esto _ r22  =  sinTheta2  *  ( cosAlpha  *  cosPhi2  +  sinPhi2 )
-            +  cosAlfa  *  cosTeta2 ;
-      esto _ r23  =  sinPhi  *  ( cosPhi  *  sinTheta * c +  sinAlpha  *  cosTheta ) ;
-      esto _ r31  =  sinPhi  *  ( cosPhi  *  cosTheta * c +  sinAlpha  *  sinTheta ) ;
-      esto _ r32  =  sinPhi  *  ( cosPhi  *  sinTheta * c -  sinAlpha  *  cosTheta ) ;
-      esto _ r33  =  cosAlfa  *  sinPhi2  +  cosPhi2 ;
-      esto _ r41  =  a1  -  a1  *  esto . r11  -  a2  *  esto . r21  -  a3  *  esto . r31 ;
-      esto _ r42  =  a2  -  a1  *  esto . r12  -  a2  *  esto . r22  -  a3  *  esto . r32 ;
-      esto _ r43  =  a3  -  a1  *  esto . r13  -  a2  *  esto . r23  -  a3  *  esto . r33 ;
-  }
+   getPolyList():any{return this.polyList;}
+   getFName():string {return this.file;}
+   getE():Point3D[] {return this.e;}
+   getVScr():Point2D[] {return this.vScr;}
+   getImgCenter():Point2D {return this.imgCenter;}
+   getRho():number{return this.rho;}
+   getD():number{return this.d;}
 
-   rotación estática ( p : Point3D ) : Point3D {
-    devolver  nuevo  Point3D (
-        pág . x  *  esto . r11  +  pag . y  *  esto . r21  +  pag . z  *  esto . r31  +  esto . r41 ,
-        pág . x  *  esto . r12  +  pag . y  *  esto . r22  +  pag . z  *  esto . r32  +  esto . r42 ,
-        pág . x  *  esto . r13  +  pag . y  *  esto . r23  +  pag . z  *  esto . r33  +  esto . r43 ) ;
-  }
+   failing(): boolean {
+      return false;
+   }
+
+   readObject(inp: Input): boolean{
+      let j = 0;
+      for (; ;){
+         let i = inp.readInt();
+         if (inp.fails()){inp.clear(); break;}
+         if (i < 0)
+         {  console.log(
+               "Negative vertex number in first part of input file");
+            return this.failing();
+         }
+        // debugger
+         //w.ensureCapacity(i + 1);
+         let x = inp.readFloat(); let y = inp.readFloat();
+         let z = inp.readFloat();
+         this.addVertex(i, x, y, z);
+         this.indices[j++]=i;
+      }
+      this.tind=j--;
+      this.shiftToOrigin(); // Origin in center of object.
+      let ch:string;
+      let count = 0;
+ 
+      do{   // Skip the line "Faces:"
+         ch = inp.readChar(); count++;
+      } while (!inp.eof() && ch != '\n');
+      
+      if (count < 6 || count > 8){
+         console.log("Invalid input file"); return this.failing();
+      }
+      //  Build polygon list:
+      for (;;){
+         let vnrs:Array<number> = [];
+         for (; ;){
+            let i = inp.readInt();
+            if (inp.fails()){inp.clear(); break;}
+            let absi = Math.abs(i);
+            if (i == 0 || absi >= this.w.length ||
+               this.w[absi] == null)
+            {  console.log("Invalid vertex number: " + absi +
+               " must be defined, nonzero and less than " + this.w.length);
+               return this.failing();
+            }
+            vnrs.push(i);
+         }
+         ch = inp.readChar();
+         if (ch != '.' && ch != '#') break;
+         // Ignore input lines with only one vertex number:
+         if (vnrs.length >= 2)
+            this.polyList.push(new Polygon3D(vnrs));
+      }
+      //inp.close();
+      //console.log(this.polyList)
+      return true;
+   }
+
+   addVertex(i: number, x: number, y: number, z: number): void{
+      if (x < this.xMin) this.xMin = x; if (x > this.xMax) this.xMax = x;
+      if (y < this.yMin) this.yMin = y; if (y > this.yMax) this.yMax = y;
+      if (z < this.zMin) this.zMin = z; if (z > this.zMax) this.zMax = z;
+      //if (i >= this.w.length) this.w.setSize(i + 1);
+      //this.w.push(new Point3D(x, y, z));
+      this.w[i] = new Point3D(x, y, z);
+   }
+
+   shiftToOrigin() :void{
+      let xwC = 0.5 * (this.xMin + this.xMax);
+      let ywC = 0.5 * (this.yMin + this.yMax);
+      let zwC = 0.5 * (this.zMin + this.zMax);
+      let n = this.w.length;
+      for (let i = 1; i < n; i++){
+         if (this.w[i] != undefined) {
+            this.w[i].x -= xwC;
+            this.w[i].y -= ywC;
+            this.w[i].z -= zwC;
+         }
+      }
+      let dx = this.xMax - this.xMin, dy = this.yMax - this.yMin, dz = this.zMax - this.zMin;
+      this.rhoMin = 0.6 * Math.sqrt(dx * dx + dy * dy + dz * dz);
+      this.rhoMax = 1000 * this.rhoMin;
+      this.rho = 3 * this.rhoMin;
+   }
+
+   initPersp(): void {
+      let costh = Math.cos(this.theta);
+      let sinth = Math.sin(this.theta);
+      let cosph = Math.cos(this.phi);
+      let sinph = Math.sin(this.phi);
+         this.v11 = -sinth; this.v12 = -cosph * costh; this.v13 = sinph * costh;
+         this.v21 = costh;  this.v22 = -cosph * sinth; this.v23 = sinph * sinth;
+                            this.v32 = sinph;          this.v33 = cosph;
+                                                       this.v43 = -this.rho;
+   }
+
+   eyeAndScreen(dim: Dimension ): number{// Called in paint method of Canvas class
+      this.initPersp();
+      let n = this.w.length;
+      this.e = new Array(n);
+      this.vScr = new Array(n);
+      let xScrMin=1e30, xScrMax=-1e30,
+            yScrMin=1e30, yScrMax=-1e30;
+      for (let i = 1; i < n; i++) {
+         let P: Point3D = this.w[i];
+         if (P == undefined) {
+            this.e[i] = undefined; this.vScr[i] = null;
+         }
+         else {
+            let x = this.v11 * P.x + this.v21 * P.y;
+            let y = this.v12 * P.x + this.v22 * P.y + this.v32 * P.z;
+            let z = this.v13 * P.x + this.v23 * P.y + this.v33 * P.z + this.v43;
+            let Pe:Point3D = this.e[i] = new Point3D(x, y, z);
+            let xScr = -Pe.x/Pe.z, yScr = -Pe.y/Pe.z;
+            this.vScr[i] = new Point2D(xScr, yScr);
+            if (xScr < xScrMin) xScrMin = xScr; 
+            if (xScr > xScrMax) xScrMax = xScr;
+            if (yScr < yScrMin) yScrMin = yScr;
+            if (yScr > yScrMax) yScrMax = yScr;
+         }
+      }
+      let rangeX = xScrMax - xScrMin, rangeY = yScrMax - yScrMin;
+      this.d = 0.95 * Math.min(dim.width/rangeX, dim.height/rangeY);
+      this.imgCenter = new Point2D(this.d * (xScrMin + xScrMax)/2,
+                              this.d * (yScrMin + yScrMax)/2);
+      for (let i = 1; i < n; i++) {
+         if (this.vScr[i] != null) { this.vScr[i].x *= this.d; this.vScr[i].y *= this.d; }
+      }
+      return this.d * Math.max(rangeX, rangeY);
+      // Maximum screen-coordinate range used in CvHLines for HP-GL
+   }
+
+   planeCoeff(): void {
+      let nFaces = this.polyList.length;
+
+      for (let j = 0; j < nFaces; j++){
+         let pol: Polygon3D  = this.polyList[j];
+         let nrs: number[] = pol.getNrs();
+         if (nrs.length < 3) continue;
+         let iA = Math.abs(nrs[0]), // Possibly negative
+             iB = Math.abs(nrs[1]), // for HLines.
+            iC = Math.abs(nrs[2]);
+         let A: Point3D  = this.e[iA], B: Point3D = this.e[iB], C: Point3D = this.e[iC];
+         let 
+            u1 = B.x - A.x, u2 = B.y - A.y, u3 = B.z - A.z,
+            v1 = C.x - A.x, v2 = C.y - A.y, v3 = C.z - A.z,
+            a = u2 * v3 - u3 * v2,
+            b = u3 * v1 - u1 * v3,
+            c = u1 * v2 - u2 * v1,
+            len = Math.sqrt(a * a + b * b + c * c), h;
+            a /= len; b /= len; c /= len;
+            h = a * A.x + b * A.y + c * A.z;
+         pol.setAbch(a, b, c, h);
+         let A1: Point2D  = this.vScr[iA], B1 = this.vScr[iB], C1 = this.vScr[iC];
+         u1 = B1.x - A1.x; u2 = B1.y - A1.y;
+         v1 = C1.x - A1.x; v2 = C1.y - A1.y;
+         if (u1 * v2 - u2 * v1 <= 0) continue; // backface
+         let inprod: number = a * this.sunX + b * this.sunY + c * this.sunZ;
+         if (inprod < this.inprodMin) this.inprodMin = inprod; 
+         if (inprod > this.inprodMax) this.inprodMax = inprod;
+      }
+      this.inprodRange = this.inprodMax - this.inprodMin;
+   }
+
+   vp( cv: CvHLines, dTheta:number, dPhi:number, fRho:number): boolean {
+      this.theta += dTheta;
+      this.phi += dPhi;
+      let rhoNew = fRho * this.rho;
+      if (rhoNew >= this.rhoMin && rhoNew <= this.rhoMax)
+         this.rho = rhoNew;
+      else
+         return false;
+      cv.paint();
+      return true;
+   }
+
+   colorCode(a: number, b: number, c: number): number{
+      let inprod = a * this.sunX + b * this.sunY + c * this.sunZ;
+      return Math.round(
+          ((inprod - this.inprodMin)/this.inprodRange) * 255);
+   }
 }
